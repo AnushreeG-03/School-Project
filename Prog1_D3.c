3c127b9f58d8ef0befc66c0bd88cd858522b44e9
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-08-18 17:33:41 +0530

    To programme Celsius into Fahrenheit

---
+++ Prog1_D3.c
@@ -0,0 +1,9 @@
+Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+#include <stdio.h>
+int main(){
+int C=100;
+int F=(C*9/5)+32;
+printf("Fahrenheit is %d\n",F);
+return 0;
+}
