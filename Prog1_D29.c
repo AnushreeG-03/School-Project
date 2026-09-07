7758b167cdf86bd858edf918c25bc9cdccdaafa1
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-07 22:22:38 +0530

    Implement sum of array elements in Prog1_D29.c

---
+++ Prog1_D29.c
@@ -0,0 +1,19 @@
+Q57: Find the sum of array elements.
+
+#include <stdio.h>
+int main(){
+int n, i, sum = 0;
+printf("Enter value: ");
+scanf("%d", &n);
+int a[n];
+for(i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+for(i = 0; i < n; i++)
+{
+sum = sum + a[i];
+}
+printf("%d", sum);
+return 0;
+}
