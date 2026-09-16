c72c58518f985c7ce805fa2fd68deb03653eddc9
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-16 12:32:21 +0530

    Matrix transpose function implementation
    Removed the implementation for transposing a matrix.

--- Prog2_D37.c
+++ Prog2_D37.c
@@ -2,26 +2,4 @@ Q74: Find the transpose of a matrix.
 
 #include <stdio.h>
 int main(){
-int a[100][100];
-int rows, cols, i, j;
-printf("Enter value: ");
-scanf("%d %d", &rows, &cols);
-for(i = 0; i < rows; i++)
-{
-for(j = 0; j < cols; j++)
-{
-scanf("%d", &a[i][j]);
-}
-}
-for(j = 0; j < cols; j++)
-{
-for(i = 0; i < rows; i++)
-{
-printf("%d", a[i][j]);
-if(i < rows - 1)
-printf(" ");
-}
-printf("\n");
-}
-return 0;
 }
