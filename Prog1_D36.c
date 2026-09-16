ed27d5c369d828f1f24b750abb597ca1372d2d47
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-16 12:29:24 +0530

    Update Prog1_D36.c

--- Prog1_D36.c
+++ Prog1_D36.c
@@ -2,26 +2,5 @@ Q71: Read and print a matrix.
 
 #include <stdio.h>
 int main(){
-int a[100][100];
-int rows, cols, i, j;
-printf("Enter value: ");
-scanf("%d %d", &rows, &cols);
-for(i = 0; i < rows; i++)
-{
-for(j = 0; j < cols; j++)
-{
-scanf("%d", &a[i][j]);
-}
-}
-for(i = 0; i < rows; i++)
-{
-for(j = 0; j < cols; j++)
-{
-printf("%d", a[i][j]);
-if(j < cols - 1)
-printf(" ");
-}
-printf("\n");
-}
-return 0;
+  
 }
