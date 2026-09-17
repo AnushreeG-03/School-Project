1f70e155e9bd9ee3d45b948a72602f2644e2fc62
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-17 22:05:00 +0530

    program

---
+++ Prog2_D39.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100][100];
+    int r, c, i, j, sum = 0;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum = sum + a[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
