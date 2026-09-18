7cd4ddf6211225801208afe270744ddd612d6929
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-18 22:47:02 +0530

    Add matrix row sum calculation
    Implement a program to calculate the sum of each row in a matrix and store it in an array.

--- Prog1_D37.c
+++ Prog1_D37.c
@@ -1,6 +1,39 @@
 Q73: Find the sum of each row of a matrix and store it in an array.
 
 #include <stdio.h>
-int main(){
 
+int main()
+{
+    int a[100][100], sum[100];
+    int rows, cols, i, j;
+
+    scanf("%d %d", &rows, &cols);
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < rows; i++)
+    {
+        sum[i] = 0;
+
+        for(j = 0; j < cols; j++)
+        {
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+
+    for(i = 0; i < rows; i++)
+    {
+        printf("%d", sum[i]);
+
+        if(i < rows - 1)
+            printf(" ");
+    }
+
+    return 0;
 }
