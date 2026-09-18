dc7308d1ae2de2f993c9904abfcc7c436f500de3
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-18 22:46:17 +0530

    Implement matrix input and output
    Added functionality to read and print a matrix.

--- Prog1_D36.c
+++ Prog1_D36.c
@@ -1,6 +1,34 @@
 Q71: Read and print a matrix.
 
 #include <stdio.h>
-int main(){
-  
+
+int main()
+{
+    int a[100][100];
+    int rows, cols, i, j;
+
+    scanf("%d %d", &rows, &cols);
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            printf("%d", a[i][j]);
+
+            if(j < cols - 1)
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
