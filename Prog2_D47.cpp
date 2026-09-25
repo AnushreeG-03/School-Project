2a4fda4e15ca5c77917eb078d88ef8202a4bd236
AnushreeG-03 <kumud.anu.329@gmail.com>
2026-09-25 23:55:33 +0530

    D47

---
+++ Prog2_D47.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[200], word[100], longest[100];
+    int i = 0, j = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    longest[0] = '\0';
+
+    while(1)
+    {
+        if(str[i] != ' ' && str[i] != '\n' && str[i] != '\0')
+        {
+            word[j] = str[i];
+            j++;
+        }
+        else
+        {
+            word[j] = '\0';
+
+            if(j > strlen(longest))
+            {
+                strcpy(longest, word);
+            }
+
+            j = 0;
+
+            if(str[i] == '\0' || str[i] == '\n')
+                break;
+        }
+
+        i++;
+    }
+
+    printf("%s", longest);
+
+    return 0;
+}
